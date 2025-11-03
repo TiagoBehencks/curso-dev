e13c750ebc559be40574ebf4cfccee7d4ada11d7
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-11-03 17:11:11 -0300

    fix: remove console

--- app/api/v1/user/route.ts
+++ app/api/v1/user/route.ts
@@ -31,7 +31,6 @@ export async function GET(request: NextRequest) {
 
     return responseWithCookies
   } catch (error) {
-    console.log('meu erro', error)
     if (error instanceof UnauthorizedError) {
       const response = NextResponse.json(
         {
